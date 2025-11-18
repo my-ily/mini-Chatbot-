d087aa00dd0afa4cf79877c5de6bf301fcbc74ba
Acey <163951804+my-ily@users.noreply.github.com>
2025-11-18 18:48:40 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Chatbotnput from './component/Chatbotnput';
+import ChatMessage from './component/ChatMessage'
+
+function App() {
+  return (
+    <div className="App">
+      <div className='bg-gray-600'>
+        <ChatMessage/>
+    <Chatbotnput/>
+
+
+
+</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Chatbotnput.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Chatbotnput() {
+  return (
+    <div>
+
+<form action="">
+
+  <input type="text" placeholder='Message...'/>
+
+<button>send</button>
+</form>
+
+    </div>
+  )
+}
+
+export default Chatbotnput
